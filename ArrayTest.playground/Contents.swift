7c958e95a3ef15996daf2fdeef796a38c1d974c4
Moritz Lugbauer <moritz.lugbauer@stud.fh-rosenheim.de>
2017-08-22 22:22:45 +0200

    nm,

--- ArrayTest.playground/Contents.swift
+++ ArrayTest.playground/Contents.swift
@@ -3,3 +3,14 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var favCany = ["100 Grand", "Milka", "bomby"]
+favCany.append("hersy")
+
+
+favCany[1] = "milka3000"
+favCany
+
+var numbers = [1,2,4,5]
+
+numbers.insert(3, at: 2)
